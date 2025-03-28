e1e7fec7ea81fe9e5178f1887e2778dc8ecb2965
Yi Pan <conlesspan@outlook.com>
2025-03-28 04:49:53 +0000

    fix: dependencies and test script.

--- playground/test.py
+++ playground/test.py
@@ -9,7 +9,6 @@ from PIL import Image
 import requests
 import sglang as sgl
 
-from sglang import global_env
 from sglang.srt.conversation import chat_templates
 from sglang.test.test_utils import is_in_ci
 from sglang.utils import async_stream_and_merge, stream_and_merge
@@ -19,18 +18,18 @@ if is_in_ci():
 
 
 def main():
-    llm = sgl.Engine(model_path="/root/models/deepseek-v2-lite", trust_remote_code=True, disable_cuda_graph=True)
+    llm = sgl.Engine(model_path="/root/models/deepseek-v2-lite-text", trust_remote_code=True, disable_cuda_graph=True)
 
     prompts = [
-        "Write a short, neutral self-introduction for a fictional character. Hello, my name is",
-        "Write a short, neutral self-introduction for a fictional character. Hello, my name is",
-        "Write a short, neutral self-introduction for a fictional character. Hello, my name is",
-        "Write a short, neutral self-introduction for a fictional character. Hello, my name is",
+        "Write a short, neutral self-introduction",
+        "for a fictional character.",
+        "Hello, my name is",
     ]
 
     sampling_params = {
         "temperature": 0.2,
         "top_p": 0.9,
+        "max_new_tokens": 0,
     }
 
     print("\n=== Testing synchronous streaming generation with overlap removal ===\n")
@@ -40,12 +39,6 @@ def main():
         merged_output = stream_and_merge(llm, prompt, sampling_params)
         print("Generated text:", merged_output)
         print()
-
-    mapping = global_env.token_to_activated_expert
-    for key, value in mapping.items():
-        print(key, value)
-    with open('data.pkl', 'wb') as f:
-        pickle.dump(mapping, f)
 
 if __name__ == "__main__":
     freeze_support()
